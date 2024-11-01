afba9ce97b615017aab6cf29c64c024e1c267cc4
anamallah <anamallah.samshulkamar@gmail.com>
2024-11-01 14:50:48 +0800

    New Login Signup

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,5 @@
 // src/pages/Home.tsx
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonInput, IonItem, IonGrid, IonRow, IonCol } from '@ionic/react';
+/*import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonInput, IonItem, IonGrid, IonRow, IonCol } from '@ionic/react';
 import React, { useState } from 'react';
 import './css/Login.css'; // Optional CSS for styling
 
@@ -15,7 +15,7 @@ const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar color="danger"> {/* Set to red */}
+        <IonToolbar color="danger"> {/* Set to red *//*}
           <IonTitle>Login</IonTitle>
         </IonToolbar>
       </IonHeader>
@@ -51,6 +51,58 @@ const Home: React.FC = () => {
               </IonCol>
             </IonRow>
           </IonGrid>
+        </div>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home; */
+
+// src/pages/Home.tsx
+import { IonContent, IonPage, IonButton, IonInput, IonItem } from '@ionic/react';
+import React, { useState } from 'react';
+import './css/Login.css'; // Optional CSS for styling
+
+const Home: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    console.log('Logging in with', email, password);
+  };
+
+  return (
+    <IonPage>
+      <IonContent fullscreen className="login-background">
+        <div className="login-container">
+          <h1 className="app-title">Monefy!</h1>
+          <h3 className="login-heading">Login</h3>
+          <IonItem className="input-item">
+            <IonInput
+              value={email}
+              placeholder="Username"
+              onIonChange={e => setEmail(e.detail.value!)}
+              required
+            />
+          </IonItem>
+          <IonItem className="input-item">
+            <IonInput
+              type="password"
+              value={password}
+              placeholder="Password"
+              onIonChange={e => setPassword(e.detail.value!)}
+              required
+            />
+          </IonItem>
+          <div className="button-container">
+            <IonButton expand="full" onClick={handleLogin} className="login-button">
+              Login
+            </IonButton>
+            <IonButton expand="full" routerLink="/signup" className="signup-button">
+              Sign Up
+            </IonButton>
+          </div>
         </div>
       </IonContent>
     </IonPage>
